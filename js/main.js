ada89354c0a723165976215b3c7d6c8a724deb8d
go1ddd <vekasio@naver.com>
2024-08-08 17:58:01 +0900

    피씨 비주얼까지 완료

---
+++ js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+    /*header start*/
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop()) {
+            $("header").addClass("on");
+        } else {
+            $("header").removeClass("on");
+        };
+    })
+
+    /*clock start*/
+    function setClock() {
+        var dateInfo = new Date();
+        var hour = modifyNumber(dateInfo.getHours());
+        var min = modifyNumber(dateInfo.getMinutes());
+        document.getElementById("time").innerHTML = hour + ":" + min;
+    }
+    function modifyNumber(time) {
+        if (parseInt(time) < 10) {
+            return "0" + time;
+        }
+        else
+            return time;
+    }
+    window.onload = function () {
+        setClock();
+        setInterval(setClock, 1000);//1초마다 setClock 함수 실행
+    }
+
+    /*intro start*/
+    var textWrapper = document.querySelector(".intro_title");
+
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+        /([^\x00-\x10]|\w)/g,
+        "<span class='letter'>$&</span>"
+    );
+
+    anime
+        .timeline({ loop: false })
+        .add({
+            targets: ".intro_title .letter",
+            translateX: [140, 0],
+            translateZ: 0,
+            opacity: [0, 1],
+            easing: "easeOutExpo",
+            duration: 1000,
+            delay: function (el, i) {
+                return 50 * i;
+
+            }
+        })
+        .add({
+            targets: ".intro_title .letter",
+            translateX: [0, -140],
+            opacity: [1, 0],
+            easing: "easeInExpo",
+            duration: 1000,
+            delay: function (el, i) {
+                return 2000 + 50 * i;
+            }
+        });
+
+    TweenMax.to(".intro_group", 2.2, {
+        delay: 3,
+        top: "-100%",
+        ease: Expo.easeInOut
+    });
+});
+
